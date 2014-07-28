cafcce73a6c13091cf870b10a27921e42dafb88b
aneisch <andrewneisch@gmail.com>
2014-07-27 19:03:30 -0500

    Added authentication

--- rubustat_web_interface.py
+++ rubustat_web_interface.py
@@ -3,10 +3,35 @@ import os
 import subprocess
 import re
 import ConfigParser
+from functools import wraps
+from flask import request, Response
 
 from getIndoorTemp import getIndoorTemp
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 app = Flask(__name__)
 #hard to be secret in open source... >.>
@@ -70,6 +95,7 @@ def getDaemonStatus():
         return "<p id=\"daemonNotRunning\"> DAEMON IS NOT RUNNING. </p>"
 
 @app.route('/')
+@requires_auth
 def my_form():
     f = open("status", "r")
     targetTemp = f.readline().strip()
@@ -102,6 +128,7 @@ def my_form():
                                         whatsOn = whatsOn)
 
 @app.route("/", methods=['POST'])
+@requires_auth
 def my_form_post():
 
     text = request.form['target']
