5f1e50d1d9a35e5fe459bb1e8959900e2c914287
aneisch <Andrew>
2014-08-03 15:03:46 -0500

    Enabled multiple sensors
    Daisy chain all the sensors you would like. Script now finds the number
    present, reads them, and averages to get a more accurate home
    temperature reading.

--- getIndoorTemp.py
+++ getIndoorTemp.py
@@ -1,30 +1,37 @@
-#!/usr/bin/python
-#Based off the tutorial by adafruit here:
-# http://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/software
-
 import subprocess
 import glob
 import time
+import os
 
 def getIndoorTemp():
+    total = 0
     subprocess.Popen('modprobe w1-gpio', shell=True)
     subprocess.Popen('modprobe w1-therm', shell=True)
+    count = subprocess.Popen('cat /sys/bus/w1/devices/w1_bus_master1/w1_master_slave_count', shell=True, stdout=subprocess.PIPE)
+    number_of_sensors = int(count.stdout.read())
     base_dir = '/sys/bus/w1/devices/'
-    device_folder = glob.glob(base_dir + '28*')[0]
-    device_file = device_folder + '/w1_slave'
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+    
+    for i in range (0,number_of_sensors):
+        device_folder = glob.glob(base_dir + '28*')[i]
+        device_file = device_folder + '/w1_slave'
         f = open(device_file, 'r')
         lines = f.readlines()
-        f.close()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_f
-     
+        f.close    
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            f2 = open(device_file, 'r')
+            lines = f.readlines()
+            f2.close()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f_i = temp_c * 9.0 / 5.0 + 32.0
+        total += temp_f_i
+    if number_of_sensors > 1:
+        average = total / number_of_sensors
+        return average
+    else:
+        return temp_f_i
+    
     return read_temp()
